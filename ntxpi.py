6d901e8e070408957a061a52455741fb87594072
lsown <aureias@gmail.com>
2020-01-05 07:48:22 -0800

    added onewire, reorganized .py structure, created archive

---
+++ ntxpi.py
@@ -0,0 +1,7 @@
+# Import Libraries
+import os
+import glob
+import time
+import onewiretemp as onewiretemp
+
+aqtemp = onewiretemp.onewiretemp()
--- ntxweb.py
+++ ntxweb.py
@@ -1,21 +1,29 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-
-class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators=[DataRequired()])
-    submit = SubmitField('Submit')
-
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -26,6 +34,10 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/')
 def index():
