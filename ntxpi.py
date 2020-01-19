8cbf25125c79e9f40d978676fc59b43e32e70d71
lsown <aureias@gmail.com>
2020-01-19 08:24:33 -0800

    GPIOs added

--- ntxpi.py
+++ ntxpi.py
@@ -3,15 +3,66 @@ import os
 import glob
 import time
 from datetime import datetime
-#import onewiretemp as onewiretemp
+
+#Rpi related objects
+
+#import RPi.GPIO as GPIO
 
 #This class instantiates several versions
 
 class aquarium:
     def __init__(self):
+        
         self.aquariumID = 100
+        import onewiretemp as onewiretemp
         self.aqtemp = onewiretemp.onewiretemp() #creates an temp object
+        self.pinsIn = {
+            5 : {'name' : 'lvlEN', 'pinType': 'levelSensor', 'state' : 1},
+            6 : {'name' : 'wastelvl', 'pinType': 'levelSensor', 'state' : 0}
+            }
+        
+        import RPi.GPIO as GPIO
+        self.pinsIn = {
+            5 : {'name' : 'lvlEN', 'pinType': 'levelSensor', 'state' : GPIO.input(5)},
+            6 : {'name' : 'wastelvl', 'pinType': 'levelSensor', 'state' : GPIO.input(6)},
+            13 : {'name' : 'cleanlvl', 'pinType': 'levelSensor', 'state' : GPIO.input(13)},
+            19 : {'name' : 'aqualvl', 'pinType': 'levelSensor', 'state' : GPIO.input(19)},
+            26 : {'name' : 'sparelvl', 'pinType': 'levelSensor', 'state' : GPIO.input(26)},
+            17 : {'name' : 'FAULTn1', 'pinType': 'motor', 'state' : GPIO.input(17)},
+            27 : {'name' : 'FAULTn2', 'pinType': 'motor', 'state' : GPIO.input(27)},
+            22 : {'name' : 'FAULTn3', 'pinType': 'motor', 'state' : GPIO.input(22)},
+            23 : {'name' : 'buttonSig', 'pinType': 'interface', 'state' : GPIO.input(23)},
+        }
+        self.pinsOut = {
+            #24 : {'name' : 'I2C RST', 'state' : 0},
+            18 : {'name' : 'LEDPwr', 'state' : 0} 
+        }
+        self.motors = {
+            drv1 : {'name' : 'aqPump', 'i2cAddress' : 0x60, 'voltage' : 0, 'direction' : 'cw'}
+            drv2 : {'name' : 'wastePump', 'i2cAddress' : 0x61, 'voltage' : 0, 'direction' : 'cw'}
+            drv3 : {'name' : 'sparePump', 'i2cAddress' : 0x62, 'voltage' : 0, 'direction' : 'cw'}
+        }
+        self.piSetup()
+        
 
+    def piSetup(self): #Sets up GPIO pins, can also add to GPIO.in <pull_up_down=GPIO.PUD_UP>
+        try:
+            GPIO.setmode(GPIO.BCM)
+            for pin in self.pinsOut:
+                GPIO.setup(pin, GPIO.OUT, initial = GPIO.LOW)
+            for pin in self.pinsIn:
+                GPIO.setup(pin, GPIO.IN)
+                GPIO.add_event_detect(pin, GPIO.FALLING, callback=self.buttonPress, bouncetime=10) 
+        except:
+            return 'Error trying to setup GPIO pins'
+
+    def buttonPress(self):
+        #GPIO.add_event_detect(channel, GPIO.RISING, callback=my_callback, bouncetime=200)
+        print('hi')
+
+    def stateMonitor(self):
+        #detects if level sensors have gone high
+        return('hi')
 
     def get_status(self):
         return {
@@ -25,3 +76,13 @@ class aquarium:
 
     def get_temp(self):
         return self.aqtemp.read_temp()[0] #0 is celcius, 1 is farenheit
+
+    def readPin(self):
+        try:
+            GPIO.setup(int(pin), GPIO.IN)
+            if GPIO.input(int(pin)) == True:
+                response = "Pin number " + pin + " is high!"
+            else:
+                response = "Pin number " + pin + " is low!"
+        except:
+          response = "There was an error reading pin " + pin + "."
