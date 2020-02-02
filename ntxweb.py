7b14db8af3ccca070772005590d4b77536913959
lsown <aureias@gmail.com>
2020-02-02 15:52:24 -0800

    fixed

--- ntxweb.py
+++ ntxweb.py
@@ -79,4 +79,4 @@ def onewire():
     return render_template('index.html')
 
 if __name__ == '__main__': 
-  socketio.run(app, host='0.0.0.0',debug=False) 
+  socketio.run(app, host='0.0.0.0',debug=True, use_reloader=False) 
