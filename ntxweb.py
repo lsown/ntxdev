1bb81b03a2e2832e8daf8ca111e20129afdf9e48
lsown <aureias@gmail.com>
2020-04-26 14:40:50 -0700

    added ntxweb

--- ntxweb.py
+++ ntxweb.py
@@ -10,7 +10,8 @@ from flask_socketio import SocketIO, emit, disconnect
 from threading import Lock
 #from flask_sqlalchemy import SQLAlchemy
 #from flask_migrate import Migrate
-import ntxpi
+#import ntxpi
+import random
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -21,62 +22,95 @@ socketio = SocketIO(app, async_mode=async_mode, cors_allowed_origins="*")
 thread = None
 thread_lock = Lock()
 
-#app.config['SQLALCHEMY_DATABASE_URI'] =\
-#    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-#db = SQLAlchemy(app)
-#migrate = Migrate(app, db)
 
-aquarium = ntxpi.aquarium()
+#aquarium = ntxpi.aquarium()
 
 # runs every x seconds and updates values on WEBUI
+aqdict = {
+	'temp': random.randrange(0,60),
+	'drv0' : True if random.randrange(0,2) == 0 else False,
+	'drv1' : True if random.randrange(0,2) == 0 else False,
+	'drv0Spd' : 0,
+	'drv1Spd' : 0,
+	'AqFlag' : random.randrange(0,2),
+	'CleanFlag' : random.randrange(0,2),
+	'WasteFlag' : random.randrange(0,2),
+	'SpareFlag' : random.randrange(0,2),
+	'exchangeState' : False
+	}
+
+aqConfig = {
+	'tempmax' : 40,
+	'tempmin' : 10
+}
+
 def aqState():
 	while True:
 		socketio.sleep(1)
-		aqdict = aquarium.get_status()
-		pinstatus = aquarium.pinsIn
+		aqdict['temp'] = random.randrange(0,30)
+		aqdict['CleanFlag'] = random.randrange(0,2)
+		aqdict['AqFlag'] = random.randrange(0,2)
+		aqdict['drv0'] = True if random.randrange(0,2) == 0 else False
+		#pinstatus = aquarium.pinsIn
 		socketio.emit('aqStatemsg', 
 			{'data' : aqdict}, 
 			namespace='/aqState')
 
 #@app.shell_context_processor
 #def make_shell_context():
-#    return dict(db=db, User=User, Role=Role)
+#	return dict(db=db, User=User, Role=Role)
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+	return render_template('404.html'), 404
 
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html'), 500
+	return render_template('500.html'), 500
 
 @app.route('/')
 def index():
-#	aqdict = {'a': 23,'b':50, 'c':30} #testercode
-	aqdict = aquarium.get_status() #returns a dictionary
-	pinstatus = aquarium.pinsIn
-	return render_template('index.html', aqdict = aqdict, pinstatus = pinstatus)
+	return render_template('index.html')
+
+@app.route('/settings')
+def settings():
+	return render_template('settings.html')
+
+@app.route('/analytics')
+def analytics():
+	return render_template('analytics.html')
 
 @socketio.on('connect', namespace='/aqState')
 def aqState_monitor():
-    global thread
-    with thread_lock:
-        if thread is None:
-            thread = socketio.start_background_task(aqState)
+	global thread
+	with thread_lock:
+		if thread is None:
+			thread = socketio.start_background_task(aqState)
 
-@socketio.on('disconnect', namespace='/test')
+@socketio.on('disconnect', namespace='/aqState')
 def test_disconnect():
-    print('Client disconnected')
-    #uncompleted
+	print('Client disconnected')
+	#uncompleted
 
-@app.route('/onewire')
-def onewire():
-    return render_template('index.html')
+@socketio.on('my_event', namespace='/aqState')
+def test_message(message):
+	#sanity check for what is received
+	for x in message['data']:
+		print('Data received: %s is %s' % (x, message['data'][x])) # sanity check for 
+		aqdict[x] = message['data'][x]
+		print('Dictionary updated: %s is %s' % (x, aqdict[x])) # sanity check for 
+	socketio.emit('aqStatemsg', {'data' : aqdict}, namespace='/aqState')
+	#aqConfig['tempmax'] = message['data']
+	#print(aqConfig['tempmax'])
+
+@socketio.on('button_event', namespace='/aqState')
+def button_message(message):
+	print(message['data']['exchange'])
+	#aqConfig['tempmax'] = message['data']
+	#print(aqConfig['tempmax'])
 
 if __name__ == '__main__': 
   socketio.run(app, host='0.0.0.0',debug=True, use_reloader=False) 
