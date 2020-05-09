01b2cd3675d5599549adce00ba4a926b718f1e11
lsown <aureias@gmail.com>
2020-05-09 15:07:41 -0700

    updated ntxpi with stepper object

--- ntxpi.py
+++ ntxpi.py
@@ -117,16 +117,6 @@ class aquarium:
                 GPIO.add_event_detect(self.pinsIn[i]['pin'], GPIO.RISING, callback=self.buttonPress, bouncetime=500) 
                 logging.info('%s set as button callback' %(str(self.pinsIn[i]['name'])))
 
-
-            #elif self.pinsIn[i]['pinType'] == 'motor':
-                #GPIO.add_event_detect(pin, GPIO.FALLING, callback=self.motorFault, bouncetime=100) 
-                #print(str(self.pinsIn[i]['pin']) + ' set as motor callback')
-    
-    def drv8830Setup(self):
-        self.drv0 = drv8830.DRV8830(i2c_addr=0x60)
-        self.drv1 = drv8830.DRV8830(i2c_addr=0x61)
-        #self.drv2 = drv8830.DRV8830(i2c_addr=0x62) #note, change HW to 0x63 to work with library
-
     def get_status(self):
         return {
             'temp': self.get_temp(), #0 is celcius, 1 is farenheit
@@ -148,6 +138,13 @@ class aquarium:
             self.display.drawStatus(text1='Temp Grab', text2=('temp: %s' %(str(self.get_temp()))))
             time.sleep(5)
 
+    def flag_hi(self):
+        for i in self.pinsIn:
+            if i['pinType'] == 'levelSensor':
+                if i['state'] == 1:
+                    return True
+
+
     def updateState(self, channel, value):
         for i in self.pinsIn:
             if channel == self.pinsIn[i]['pin']:
@@ -162,23 +159,22 @@ class aquarium:
                 GPIO.output(self.pinsOut['LEDPwr']['pin'], 1)
                 self.pinsIn['buttonSig']['priorState'] = 1
                 self.display.drawStatus(text1='pumping', text2=('temp: %s' %(str(self.get_temp()))))
-                self.stepMotor.stepInfinite()
+                
+                if self.pinsIn['aquaFlag']['state'] == 0:
+                    self.stepMotor.stepInfinite('cw')
+                else:
+                    pass
+                    logging.info('Aqua Flag still high, motor remains disabled')              
                 '''
+                #old code for running
                 motorThread = threading.Thread(target=self.drv8825, args=(600,0,10000,), daemon=True)
                 motorThread.start()
                 '''
-                #self.drv8825(frequency = 600, direction = 1, steps = 10000)
-                #self.motorControl(name='drv0', speed = 1, direction = 'forward')
             else:
                 GPIO.output(self.pinsOut['LEDPwr']['pin'], 0)
                 self.pinsIn['buttonSig']['priorState'] = 0
-                self.display.drawStatus(text1='NTXPi Ready!', text2=('temp: %s' %(str(self.get_temp()))))
-                
-                self.stepMotor.disableMotor()
-
-                #self.drv8825(0, 0, 0, disable=True)
-                
-                #self.motorControl(name='drv0', speed = 0, direction = 'brake')
+                self.display.drawStatus(text1='NTXPi Ready!', text2=('temp: %s' %(str(self.get_temp()))))    
+                self.stepMotor.disableMotor() #disables stepper
             print('LED state changed to %s' %(str(self.pinsIn['buttonSig']['priorState'])))
             self.buttonTime = time.time() #sets a time for last button press
         global exit_loop
@@ -188,30 +184,10 @@ class aquarium:
         if GPIO.input(channel) == 1:
             self.updateState(channel, 1)
             #self.motorControl(name='drv0', speed=0, direction = 'brake')
-            self.drv8825(0,0,0,disable=True)
+            self.stepMotor.disableMotor() #disables stepper
             self.display.drawStatus(text1='Full Aquarium', text2=('temp: ' + str(self.get_temp())))
         if GPIO.input(channel) == 0:
             self.updateState(channel, 0)
-
-    def drv8825(self, frequency, direction, steps, disable = False, dutyCycle = 50, stepEnPin = 20, stepDirPin = 21, stepStepPin = 18):
-        if disable == True: #disables motor
-            GPIO.output(stepEnPin, 1) #disables motor power by pulling pin HI
-            logging.info("motor disable triggered, turning motor off")
-        else:
-            stepTime = 1/frequency/2 #duration for high, duration for low
-            totalTime = 1/frequency * steps #calculates total estimated time for routine to finish
-            GPIO.output(stepDirPin, direction)
-            GPIO.output(stepEnPin, 0) #LOW enable drv8825 chip
-            logging.info("Stepper enabled, estimated time %s" %(str(totalTime)))
-            count = 0
-            while count <= steps:
-                GPIO.output(stepStepPin, 1)
-                time.sleep(stepTime)
-                GPIO.output(stepStepPin,0)
-                time.sleep(stepTime)
-                count += 1
-            GPIO.output(stepEnPin,1) #disable stepper power
-            logging.info("Steppers finished %s steps at frequency %s, stepper disabled" % (count-1, frequency))
 
     def motorFault(self, channel):
         #GPIO.add_event_detect(channel, GPIO.RISING, callback=my_callback, bouncetime=200)
@@ -238,6 +214,10 @@ class aquarium:
         self.motors[name]['direction'] = direction
         self.motors[name]['state'] = (direction + " direction @ speed " + str(speed))
 
+    def drv8830Setup(self):
+        self.drv0 = drv8830.DRV8830(i2c_addr=0x60)
+        self.drv1 = drv8830.DRV8830(i2c_addr=0x61)
+        #self.drv2 = drv8830.DRV8830(i2c_addr=0x62) #note, change HW to 0x63 to work with library
 
 '''
 class myThread (threading.Thread):
@@ -307,7 +287,7 @@ class stepMotor:
         self.pwm.start(self.dutyCycle)
         timerThread.start()
 
-    def stepInfinite(self):
+    def stepInfinite(self, rotation):
         self.enableMotor()
         self.pwm.start(self.dutyCycle)
 
